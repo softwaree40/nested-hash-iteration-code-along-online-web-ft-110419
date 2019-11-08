8550587a955608db77577bc6e7bc19e7889e7142
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 07:02:23 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,16 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+    contacts.each do |persons,data|
+      if persons == "Freddy Mercury"
+        data.each do |att ,value|
+          if att == :favorite_ice_cream_flavors
+            value.delete_if {|ice_cream|ice_cream == "strawberry"}
+              
+          # binding.pry
+               
+             end
+          end
+       end
+    end
 end
