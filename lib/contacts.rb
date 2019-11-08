bc3395d0a7ed1f7c1b8338c6ed8cbb8451033d98
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 07:01:40 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,10 +19,10 @@ def remove_strawberry(contacts)
       if persons == "Freddy Mercury"
         data.each do |att ,value|
           if att == :favorite_ice_cream_flavors
-            value.delete_by do |ice_cream|
-              ice_cream == "strawberry"
-           binding.pry
-               end
+            value.delete_by {|ice_cream|ice_cream == "strawberry"}
+              
+          # binding.pry
+               
              end
           end
        end
